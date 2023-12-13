1faace9dbf92978d4e848b0f476ff312fcdce70e
Campezí del frau <84505288+Aran-Estera-Puig@users.noreply.github.com>
2023-12-13 19:52:08 +0100

    Update and rename Geo_something_more.py to Georun.py

---
+++ Georun.py
@@ -0,0 +1,11 @@
+import time, random, string
+
+print("Welcome to Georun\nChoose a language:\nEnglish | Español | Català")
+
+language = input()
+
+if language.lower == "english":
+
+elif language.lower == "español":
+
+elif language.lower == "català":
